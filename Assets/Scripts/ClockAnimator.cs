66d0a7a4f5dc442db0857ec2cd89e75b8be78976
Agro Rachmatullah <agro1986@gmail.com>
2016-10-01 04:48:55 +0900

    initial commit

---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public Transform hours;
+	public Transform minutes;
+	public Transform seconds;
+
+	public bool analog = true;
+
+	private const float degreesPerHour = 360.0f / 12.0f;
+	private const float degreesPerMinute = 360.0f / 60.0f;
+	private const float degreesPerSecond = 360.0f / 60.0f;
+
+	// Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		if (analog) {
+			UpdateAnalog ();
+		} else {
+			UpdateDigital ();
+		}
+	}
+
+	private void UpdateAnalog() {
+		var timeOfDay = DateTime.Now.TimeOfDay;
+		hours.localRotation = Quaternion.Euler (0.0f, 0.0f, -(float)timeOfDay.TotalHours * degreesPerHour);
+		minutes.localRotation = Quaternion.Euler (0.0f, 0.0f, -(float)timeOfDay.TotalMinutes * degreesPerMinute);
+		seconds.localRotation = Quaternion.Euler (0.0f, 0.0f, -(float)timeOfDay.TotalSeconds * degreesPerSecond);
+	}
+
+	private void UpdateDigital() {
+		var time = DateTime.Now;
+		hours.localRotation = Quaternion.Euler (0.0f, 0.0f, -time.Hour * degreesPerHour);
+		minutes.localRotation = Quaternion.Euler (0.0f, 0.0f, -time.Minute * degreesPerMinute);
+		seconds.localRotation = Quaternion.Euler (0.0f, 0.0f, -time.Second * degreesPerSecond);
+	}
+}
---
+++ Assets/Scripts/fps.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class fps : MonoBehaviour
+{
+	float deltaTime = 0.0f;
+	float msec = 0.0f;
+	float framePerSecond = 0.0f;
+	string fpsText = "";
+	DateTime lastUpdate = DateTime.Now;
+
+	void Update()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		float newMsec = deltaTime * 1000.0f;
+		msec += (newMsec - msec) * 0.1f;
+		float newFPS = 1.0f / deltaTime;
+		framePerSecond += (newFPS - framePerSecond) * 0.1f;
+	}
+
+	void OnGUI()
+	{
+		DateTime newNow = DateTime.Now;
+		var interval = newNow - lastUpdate;
+		if (interval.TotalSeconds >= 0.3) {
+			lastUpdate = newNow;
+			fpsText = string.Format("{0:0.0} ms ({1:0.} fps)", msec, framePerSecond);
+		}
+
+		int w = Screen.width, h = Screen.height;
+		GUIStyle style = new GUIStyle();
+		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 3 / 100;
+		style.normal.textColor = new Color (1.0f, 0.0f, 0.5f, 1.0f);
+		GUI.Label(rect, fpsText, style);
+	}
+}
